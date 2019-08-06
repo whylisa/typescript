83cda277994261a8de323069c44cdf1b570390c3
haoyue.wang <843655240@qq.com>
2019-08-06 18:30:01 +0800

    axios

--- axios.ts
+++ axios.ts
@@ -15,3 +15,92 @@ function createInstance(): AxiosInstance {
 
 const axios = createInstance()
 export default axios
+
+export type Method = {
+	| 'get'
+	| 'GET'
+	| 'delete'
+	|
+	|
+	|
+	|
+	|
+	|
+	|
+	|
+	|
+	|
+	|
+	|
+	
+}
+
+export interface AxiosRequestConfig {
+	url? string
+	method?: Method
+	data?: any
+	params?:any
+	responseType?: XMLHttpRequestResponseType
+	timeout?: number
+}
+
+export interface AxiosResponse<T = any> {
+	data: T
+	status: number
+	statusText: string
+	headers: any
+	configL: AxiosRequestConfig
+	request: any
+}
+
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>>{}
+export interface AxiosError extends Error {
+	config: AxiosRequestConfig
+	code?: string
+	request?: any
+	response?: AxiosResponse
+	isAxiosError: boolean
+}
+export interface Axios {
+	interceptors: {
+		request: AxiosInterceptorManage<AxiosRequestConfig>
+		response: AxiosInterceptorManage<AxiosResponse>
+	}
+	
+	request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+	get<T = any>(url: string,config?: AxiosRequestConfig): AxiosPromise<T>
+	delete<T = any>(url:string,config?: AxiosRequestConfig): AxiosPromise<T>
+	head<T = any>(url: string,config?: AxiosRequestConfig): AxiosPromise<T>
+	options<T = any>(url:string, config?: AxiosRequestConfig): AxiosPromise<>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
