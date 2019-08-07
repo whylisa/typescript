d11995ca5aeb30c58548a968021859bbf2b0b74d
haoyue.wang <843655240@qq.com>
2019-08-07 11:34:40 +0800

    axios post get

--- axios.ts
+++ axios.ts
@@ -223,8 +223,100 @@ function transformResponseData(res: AxiosResponse): AxiosResponse {
 	res.data = transformResponse(res.data)
 	return res
 }
+interface Interceptors {
+	request: InterceptorManager<AxiosRequestConfig>
+	response: InterceptorManager<AxiosResponse>
+}
+interface PromiseChain<T> {
+	resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
+	rejected?: RejectedFn
+}
 
-
+export default class Axios {
+	interceptors: Interceptors
+	
+	constructor() {
+		this.interceptors = {
+			request: new InterceptorManager<AxiosRequestConfig>(),
+			response: new InterceptorManager<AxiosResponse>()
+		}
+	}
+	
+	request( url: any,config?: any): AxiosPromise {
+		if(typeof url === 'string') {
+			if(!config) {
+				config = {}
+			}
+			config.url = url
+		}else {
+			config = url 
+		}
+		const chain: PromiseChain<any>[] = [
+		  {
+		  	resolved: dispatchRequst,
+		  	rejected: undefined
+		  }
+		]
+		this.interceptors.reqeust.forEach(interceptor => {
+			chain.unshift(interceptor)
+		})
+		this.interceptors.response.forEach(interceptor => {
+			chain.push(interceptor)
+		})
+		
+		let promise = Promise.resolve(config)
+		while (chain.length) {
+			const { resolved, rejected} = chain.shift()!
+			promise = promise.then(resolved,rejected)
+		}
+		return promise
+	}
+	get(url: string,config?: AxiosRequestConfig): AxiosPromise {
+		return this._requestMethodWithoutData('get',url,config)
+	}
+	
+	delete(url: string,config?: AxiosRequestConfig): AxiosPromise {
+		return this._requestMethodWithoutData('delete',url,config)
+	}
+	head(url: string,config?: AxiosRequestConfig): AxiosPromise {
+		return this._requestMethodWithoutData('head',url,config)
+	}
+	options(url: string,config?: AxiosRequestConfig): AxiosPromise {
+		return this._requestMethodWithoutData("options",url,config)
+	}
+	post(url:string,data?: any,config?: AxiosRequestConfig): AxiosPromise {
+		return this._requestMethodWithoutData('post',url,data,config)
+	}
+	put 
+	petch
+	_requestMethodWithoutData(
+		method: Method,
+		url: string,
+		config?:AxiosRequestConfig
+	): AxiosPromise {
+		return this.reqeust(
+			
+			Object.assign(config || {} ,{
+				method,
+				url
+			})
+		)
+	}
+	_requestMethodWithoutData(
+		method: Method,
+		url: string,
+		data?: any,
+		config?: AxiosRequestConfig
+	): AxiosPromise{
+		return this.request(
+			Object.assign(config || {} ,{
+				method,
+				url,
+				data
+			})
+		)
+	}
+}
 
 
 
