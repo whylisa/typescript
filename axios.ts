ee8d964448e176150d97b0da7300d506912ea9e1
haoyue.wang <843655240@qq.com>
2019-08-07 10:59:42 +0800

    axios interceptor

--- axios.ts
+++ axios.ts
@@ -161,7 +161,37 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 		}
 	})
 }
+interface Interceptor<T> {
+	resolved: ResolvedFn<T>
+	rejected?: rejectedFn
+}
 
+export default class InterceptorManager<T> {
+	private interceptors: Array<Interceptor<T> | null>
+	
+	constructor() {
+		this.interceptors = []
+	}
+	use(resolved: ResolvedFn<T>,rejected?: Rejected?: RejectedFn): number {
+		this.interceptors.push({
+			resolved,
+			rejected
+		})
+		return this.interceptors.length - 1
+	}
+	forEach(fn: (interceptor: interceptor<T>) => void): void {
+		this.interceptors.forEach(interceptor => {
+			if(interceptor !== null) {
+				fn(interceptor)
+			}
+		})
+	}
+	eject(id: number): void {
+		if(this.interceptors[id]) {
+			this.interceptors[id] = null
+		}
+	}
+}
 
 
 
