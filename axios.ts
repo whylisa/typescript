be5554fed846dace1b616691894411231a16aa8f
haoyue.wang <843655240@qq.com>
2019-08-07 11:08:09 +0800

    axios dispatch

--- axios.ts
+++ axios.ts
@@ -194,8 +194,35 @@ export default class InterceptorManager<T> {
 }
 
 
+export default function dispatchRequest(config: AxiosRequestConfig) : AxiosPromise{
+	processConfig(config)
+	return xhr(config).then(res => {
+		return transformResponseData(res)
+	})
+	
+}
 
+function processConfig(config: AxiosRequestConfig): void {
+	config.url = transfromURL(config)
+	config.headers = taransformHeaders(config)
+	config.data = tansformRequestData(config)
+}
+function transformURL(config: AxiosRequestConfig): string{
+	const ( url , parms ) = config
+	return buildURL(url !,params)
+}
+function transformRequstData(config:AxiosRequestConfig): any {
+	return transformRequest(config.data)
+}
+function transformHeaders(config:AxiosRequstConfig): any{
+	const { headers = {},data } = config
+	return processHeaders(headers,data)
+}
 
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+	res.data = transformResponse(res.data)
+	return res
+}
 
 
 
