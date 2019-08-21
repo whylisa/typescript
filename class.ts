e6d744bdd380276500bddd65813a22006eb9799f
haoyue.wang <843655240@qq.com>
2019-08-21 18:36:56 +0800

    lei

--- class.ts
+++ class.ts
@@ -33,3 +33,23 @@ class User {
 interface Person {
 	firstName
 }
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+class Greeter {
+	greeting: string;
+	constructor(message: string) {
+		this.greeting = message
+	}
+	greet() {
+		return "hello" + this.greeting;
+	}
+}
