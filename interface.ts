6531ac9ea2232b236bc082118322a62d187023f3
haoyue.wang <843655240@qq.com>
2019-08-22 11:42:09 +0800

    接口

--- interface.ts
+++ interface.ts
@@ -10,3 +10,32 @@ function greeter(person:Person) {
 let user = { firstName: 'june',lastName: "user"}
 
 document.body.innerHTML = greeter(user)
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+  let newSquare = {color: "white", area: 100};
+  if (config.clor) {
+    // Error: Property 'clor' does not exist on type 'SquareConfig'
+    newSquare.color = config.clor;
+  }
+  if (config.width) {
+    newSquare.area = config.width * config.width;
+  }
+  return newSquare;
+}
+
+let mySquare = createSquare({color: "black"});
+
+
+interface SquareConfig {
+	color?: string;
+	width?: number
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number} {
+	let newSquar = { color: 'white', area: 100}
+}
