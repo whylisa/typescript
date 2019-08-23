3b929d3bfd8e074a1c6ac57c2bff959fc4f8bd7a
haoyue.wang <843655240@qq.com>
2019-08-23 17:03:59 +0800

    交叉类型

--- 交叉类型.ts
+++ 交叉类型.ts
@@ -1,3 +1,34 @@
 function extend<T,U>(first: T,second: U): T & U {
 	let result = {} as T & U
+	for (let id in first) {
+		result[id] = first[id] as any
+	}
+	for (let id in second) {
+		if(!result.hasOwnProperty(id)) {
+			result[id] = second[id] as any
+		}
+	}
+	return result
+}
+
+class Person {
+	constructor (public name: string) {
+		
+	}
+}
+
+interface loggable {
+	log() : void
+}
+
+class ConsoleLogger implements Loggle {
+	log() {
+		
+	}
+}
+
+var jim = extend(new Person('Jim'),new ConsoleLogger())
+
+function padLeft(value: string,padding:any) {
+	if(typeof padding)
 }
