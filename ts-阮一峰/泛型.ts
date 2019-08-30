d2aca3248ab63621dfe45acf2947107898663629
haoyue.wang <843655240@qq.com>
2019-08-30 15:38:09 +0800

    泛型约束

--- ts-阮一峰/泛型.ts
+++ ts-阮一峰/泛型.ts
@@ -9,7 +9,16 @@ function createArray(length: number,value: string) Array<any> {
 createArray(3,'xxx')
 Array<any>  //为任意类型，这时候我们需要泛型来指定返回值的类型
 
-function createArray(length: number,value: T) Array<T> {
+function createArray<T>(length: number,value: T) Array<T> {
 	let result: T[] = []
 	//code
+}
+
+//泛型约束
+interface lengthWise {
+	length: number;
+}
+function loggingIndentity<T extends lengthWise>(arg: T): T {
+	console.log(arg.length)
+	return arg
 }
